00c91b502c061e7d136164dd20399a9237cf8f95
rdeavila <richard.deavila@mobileforming.com>
2020-01-01 19:13:50 -0800

    Created template for displaying messages

--- public/js/chat.js
+++ public/js/chat.js
@@ -1,25 +1,53 @@
 const socket = io();
 
+const $form = document.querySelector("#message-form");
+const $messageFormInput = $form.querySelector("input");
+const $messageFormButton = $form.querySelector("button");
+const $sendLocation = document.querySelector("#send-location");
+const $messages = document.querySelector("#messages");
+
+// Templates
+const messageTemplate = document.querySelector("#message-template").innerHTML;
+
 socket.on("message", message => {
   console.log(message);
+  const html = Mustache.render(messageTemplate, { message });
+  $messages.insertAdjacentHTML("beforeend", html);
 });
 
-
-document.querySelector("#message-form").addEventListener("submit", e => {
+$form.addEventListener("submit", e => {
   e.preventDefault();
+
+  // disable the form
+  $messageFormButton.disabled = true;
 
   const message = e.target.elements.message.value;
 
-  socket.emit("sendMessage", message);
+  socket.emit("sendMessage", message, error => {
+    // re-enable the form
+    $messageFormButton.disabled = false;
+    $messageFormInput.value = "";
+    $messageFormInput.focus();
+    if (error) {
+      return console.log(error);
+    }
+    console.log("message delivered");
+  });
 });
 
-document.querySelector("#send-location").addEventListener("click", e => {
+$sendLocation.addEventListener("click", e => {
+  $sendLocation.disabled = true;
+
   if (!navigator.geolocation) {
+    $sendLocation.disabled = false;
     return alert("Geolocation is not supported by your browser");
   }
 
   navigator.geolocation.getCurrentPosition(position => {
     const { latitude, longitude } = position.coords;
-    socket.emit("sendLocation", { latitude, longitude });
+    socket.emit("sendLocation", { latitude, longitude }, () => {
+      $sendLocation.disabled = false;
+      console.log("Location shared!");
+    });
   });
 });
--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ const path = require("path");
 const http = require("http");
 const express = require("express");
 const socketio = require("socket.io");
+const Filter = require("bad-words");
 
 const app = express();
 const server = http.createServer(app);
@@ -18,16 +19,23 @@ io.on("connection", socket => {
   socket.emit("message", "Welcome!");
   socket.broadcast.emit("message", "A new user has joined!");
 
-  socket.on("sendMessage", message => {
+  // The callback function is the acknowledgement callback. You can also pass args to the callback
+  socket.on("sendMessage", (message, callback) => {
+    const filter = new Filter();
+    if (filter.isProfane(message)) {
+      return callback("Profanity is not allowed!");
+    }
     io.emit("message", message);
+    callback();
   });
 
   socket.on("disconnect", () => {
     io.emit("message", "A user has left!");
   });
 
-  socket.on("sendLocation", ({ latitude, longitude }) => {
-    io.emit("message", `${longitude},${latitude}`);
+  socket.on("sendLocation", ({ latitude, longitude }, callback) => {
+    io.emit("message", `https://google.com/maps?q=${longitude},${latitude}`);
+    callback();
   });
 });
 
